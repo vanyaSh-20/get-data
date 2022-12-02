b29d05e7e195cbf42e2597a6e4cddeb5b9af9d8d
vanyaSh-20 <110335977+vanyaSh-20@users.noreply.github.com>
2022-12-02 13:05:15 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  const[data,setData] = useState(null)
+
+  const getData= (e) => {
+    setData(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Using onChange function printing value</h1>
+      <input type="text" onChange={getData}/>
+      <p>{data}</p>
     </div>
   );
 }
